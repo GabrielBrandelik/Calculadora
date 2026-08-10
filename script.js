67d0032a9be1dde4489d736f4d9b880180b31620
Gabriel Brandelik <g.choma@escola.pr.gov.br>
2026-08-10 11:56:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 function insert(num) {
-    var numero = document.getElementById ('resultado').innerHTML;
-    document.getElementById ('resultado').innerHTML = numero + num;
-
+    var resultado = document.getElementById('resultado');
+    if (resultado.innerHTML === "Nada para calcular") {
+        resultado.innerHTML = "";
+    }
+    resultado.innerHTML += num;
 }
 
 function clean() {
-    document.getElementById ('resultado').innerHTML = " ";
-
+    document.getElementById('resultado').innerHTML = "";
 }
 
 function back() {
-    var resultado = document.getElementById ('resultado').innerHTML;
-    document.getElementById ('resultado').innerHTML = resultado.substring (0, resultado.length -1);
-
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
 }
 
-function calcular () {
-    var resultado = document.getElementById ('resultado').innerHTML;
-    if(resultado) {
-        document.getElementById('resultado').innerHTML=eval(resultado);
-    }
-    else{
+function calcular() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado && resultado !== "Nada para calcular") {
+        try {
+            document.getElementById('resultado').innerHTML = eval(resultado);
+        } catch (e) {
+            document.getElementById('resultado').innerHTML = "Erro";
+        }
+    } else {
         document.getElementById('resultado').innerHTML = "Nada para calcular";
     }
 }
